280e229a41c941aad0e33140321dd9faacb9173d
DamianSikorski <damian.sikorski671@gmail.com>
2022-10-29 01:50:34 +0200

    mod z 10

--- js/task-10.js
+++ js/task-10.js
@@ -2,6 +2,7 @@ const createBtn = document.querySelector(`[data-create]`);
 const destroyBtn = document.querySelector(`[data-destroy]`);
 const boxesDiv = document.querySelector("#boxes");
 const input = document.querySelector(`[type="number"]`);
+const info = document.querySelector(".info");
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -10,19 +11,29 @@ function getRandomHexColor() {
 const createBoxes = () => {
   const numberOfBoxes = Math.floor(input.value);
   let dimensions = 30;
-  for (let i = 0; i < numberOfBoxes; i++) {
-    const div = document.createElement("div");
-    boxesDiv.appendChild(div);
-    div.style.width = `${dimensions}px`;
-    div.style.height = `${dimensions}px`;
-    div.style.background = `${getRandomHexColor()}`;
-    dimensions = dimensions + 10;
+
+  if (input.value == "") {
+    info.textContent = "Enter the number before clicking this button!";
+    info.style.color = "red";
+  } else {
+    for (let i = 0; i < numberOfBoxes; i++) {
+      const div = document.createElement("div");
+      boxesDiv.appendChild(div);
+      div.style.width = `${dimensions}px`;
+      div.style.height = `${dimensions}px`;
+      div.style.background = `${getRandomHexColor()}`;
+      dimensions = dimensions + 10;
+      info.textContent = `Wow! You just created ${input.value} Divs 👻`;
+      info.style.color = `green`;
+    }
   }
 };
 
 const destroyBoxes = () => {
   while (boxesDiv.firstChild) {
     boxesDiv.removeChild(boxesDiv.lastChild);
+    info.textContent = `Huh... it seems like you destroyed all divs! 🤔`;
+    info.style.color = "black";
   }
 };
 
